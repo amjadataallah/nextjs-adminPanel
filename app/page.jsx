f506314095f052b9dbf96c69f6a6c798c63d2b25
Amjad Ata-allah <ataallah.amjad@gmail.com>
2023-09-10 19:05:37 +0300

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
+        debitis excepturi ab aliquam iure, voluptatibus aut pariatur magni unde
+        dignissimos corporis quod cumque, at animi ad tenetur rerum qui
+        quibusdam quisquam officiis dolorem eos itaque laudantium. Quo mollitia,
+        reprehenderit ipsam molestias deleniti, ea officia perferendis error
+        nihil sed laudantium aspernatur?
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
