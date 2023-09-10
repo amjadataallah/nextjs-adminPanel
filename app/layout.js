b3231accbfdf55c63880c6f26d2284929234a506
Amjad Ata-allah <ataallah.amjad@gmail.com>
2023-09-10 23:16:35 +0300

    feat: Create Basic Structure

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,20 @@
-import './globals.css'
-
+import MainLayout from "@/components/MainLayout";
+import "./globals.css";
+import MenuContextProvider from "@/context/MenuContext";
 
 export const metadata = {
-  title: 'Illiya-Coding Admin Panel',
-  description: 'This is a nextjs 13 AdminPanel created by Illiya-coding.',
-}
+  title: "Illiya-Coding Admin Panel",
+  description: "This is a nextjs 13 AdminPanel created by Illiya-coding.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body>
+        <MenuContextProvider>
+          <MainLayout>{children}</MainLayout>
+        </MenuContextProvider>
+      </body>
     </html>
-  )
+  );
 }
